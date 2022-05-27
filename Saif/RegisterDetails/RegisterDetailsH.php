b5ead8a4b86f8989d28a28da475e5343f121dfe1
elmoushy <seif778811@gmail.com>
2022-05-28 00:18:21 +0200

    m

--- Saif/RegisterDetails/RegisterDetailsH.php
+++ Saif/RegisterDetails/RegisterDetailsH.php
@@ -1,25 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="TemplateMo">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" >
     <title>Education Meeting HTML5 Template</title>
-
     <!-- Bootstrap core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="../assets/css/fontawesome.css">
     <link rel="stylesheet" href="../assets/css/templatemo-edu-meeting.css">
     <link rel="stylesheet" href="../assets/css/owl.css">
     <link rel="stylesheet" href="../assets/css/lightbox.css">
-
   <!-- ***** Main Banner Area Start ***** -->
   <section class="section main-banner" id="top" data-section="section1">
       <video autoplay muted loop id="bg-video">
@@ -29,7 +23,6 @@
         <br/>
         <br/>
         <h1 style="color:rgb(233, 242, 240);"align="RIGHT"> <pre> Helwan University  </pre></h1>
-        
           <br/>
           <br/>
           <br/>
@@ -45,20 +38,17 @@
                 <div style="color:white">
                 <h1> <a href='../Register/Register.html'>Register Details</a> : <?php echo $_GET["ID"];?></h1>
                 </div>
-
               <br/>
              <br/>
               <div class="content">
                 <input type="number" name="ID" placeholder="RegisterDetails Id"> <br>
                  <input type="hidden" name="rgID" value="<?php echo $_GET["ID"]?>"> <br>
-                 
                  <select name="Crs" >
                   <option value="0">Non</option>
                   <?php 
                       include_once "../functions.php";
                       $File = new filemanager();
                       $File->setFilenames("courses");
-                      
                       $List = $File->AllContents();
                       for ($i=0; $i < count($List)  - 1; $i++) { 
                           $Array = explode("~",$List[$i]);
