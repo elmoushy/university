530ec3ac7964c7afd42e2fffdc1b21f8032c746a
afawzy210 <100765824+afawzy210@users.noreply.github.com>
2022-05-28 00:13:17 +0200

    /

--- Saif/Interface.php
+++ Saif/Interface.php
@@ -12,4 +12,37 @@ interface decorator {
     public function TotalCost(float $Cost);
 }
 
+abstract class observer
+{
+    private $subject;
+
+    public function __construct($subject)
+    {
+        $this->subject = $subject;
+
+        $this->subject->attach($this);
+    }
+
+    public abstract function update(); 
+}
+
+class Subject
+{
+    private $Observer = [];
+    
+    public function attach($Observer)
+    {
+        array_push($this->Observer,$Observer);
+    }
+
+    public function notifyAllObserv()
+    {
+        foreach ($this->Observer as $Item )
+        {
+            $Item->update();
+        }
+    }
+
+}
+
 ?>
---
+++ Saif/observer/NewUserN.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("../Interface.php");
+
+class NewUserN extends observer
+{
+    function update()
+    {
+        //testing
+        echo ("id name pass email");
+    }
+}
+
+?>
